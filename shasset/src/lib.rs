874df910775123e1ccf5082250e57b0fd1137785
phlax <phlax@users.noreply.github.com>
2026-07-17 14:18:20 +0100

    botforge: Separate unsafe/plugin code (#443)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- shasset/src/lib.rs
+++ shasset/src/lib.rs
@@ -1,3 +1,4 @@
+#![forbid(unsafe_code)]
 pub mod cli;
 pub mod fetch;
 pub mod manifest;
--- shasset/src/main.rs
+++ shasset/src/main.rs
@@ -1,3 +1,4 @@
+#![forbid(unsafe_code)]
 use anyhow::{bail, Context, Result};
 use clap::Parser;
 use rayon::prelude::*;
